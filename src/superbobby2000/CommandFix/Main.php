7a4ef33ea72137f1bbc5e5ef3565c86b23189683
superbobby2000 <coudercleo10@gmail.com>
2021-06-11 18:13:36 -0400

    fixed new glitches
    /\spawn
    /"spawn"

--- src/superbobby2000/CommandFix/Main.php
+++ src/superbobby2000/CommandFix/Main.php
@@ -16,12 +16,14 @@ class Main extends PluginBase implements Listener {
 
     public function onCommandExecute(PlayerCommandPreprocessEvent $event) {
         $message = $event->getMessage();
-        $msg = explode(' ',trim($message));
+        $msg = explode(' ', trim($message));
         $m = substr("$message", 0, 1);
         $whitespace_check = substr($message, 1, 1);
         $slash_check = substr($msg[0], -1, 1);
+        $quote_mark_check = substr($message, 1, 1) . substr($message, -1, 1);
+
         if ($m == '/') {
-            if ($whitespace_check === ' ' or $slash_check === '\\') {
+            if ($whitespace_check === ' ' or $whitespace_check === '\\' or $slash_check === '\\' or $quote_mark_check === '""') {
                 $event->setCancelled();
             }
         }
